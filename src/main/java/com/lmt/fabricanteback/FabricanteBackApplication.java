f903c7798ae535e82f3709ea11ff3ce237e1c351
carlos <celiberato@hotmail.com>
2025-06-04 18:17:58 -0300

    MVP 1 - bug fix - security

--- src/main/java/com/lmt/fabricanteback/FabricanteBackApplication.java
+++ src/main/java/com/lmt/fabricanteback/FabricanteBackApplication.java
@@ -2,8 +2,9 @@ package com.lmt.fabricanteback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class FabricanteBackApplication {
 
 	public static void main(String[] args) {
